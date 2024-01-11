e41a4c9e410d136ccfe93c42a08e4f5d1231edfe
Tyler Brinks <tylerbrinks@hotmail.com>
2024-01-10 20:33:17 -0700

    io

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get("/test", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
    });
 // Enable CORS
-//io.set("origins", "*:*");
+io.set("origins", "*:*");
 
 //
 server.listen(port);
@@ -26,55 +26,55 @@ server.listen(port);
 console.log("* Server started on " + port);
 
 //
-// io.on("connection", function (socket)
-// {
-//     console.log('connection!');
+io.on("connection", function (socket)
+{
+    console.log('connection!');
 
-//     socket.on("enter", (data) =>
-//     {
-//         console.log('enter!');
-//         console.log(data);
-//         socket.join(data.roomId);
-//         socket.roomId = data.roomId;
+    socket.on("enter", (data) =>
+    {
+        console.log('enter!');
+        console.log(data);
+        socket.join(data.roomId);
+        socket.roomId = data.roomId;
 
-//         const room = io.of("/").in().adapter.rooms[socket.roomId];
-//         const config = peerConfig.get(data.roomId, process.env.TURN_SECRET);
+        const room = io.of("/").in().adapter.rooms[socket.roomId];
+        const config = peerConfig.get(data.roomId, process.env.TURN_SECRET);
 
-//         socket.emit("sockets", {
-//             sockets: room.sockets,
-//             peerConfig: config,
-//         });
+        socket.emit("sockets", {
+            sockets: room.sockets,
+            peerConfig: config,
+        });
 
-//         console.log("enter", socket.roomId, room, config);
-//     });
+        console.log("enter", socket.roomId, room, config);
+    });
 
-//     socket.on("disconnect", () =>
-//     {
-//         const sockets = io.of("/").in().adapter.rooms[socket.roomId];
+    socket.on("disconnect", () =>
+    {
+        const sockets = io.of("/").in().adapter.rooms[socket.roomId];
 
-//         console.log("disconnect", socket.id, socket.roomId, sockets);
+        console.log("disconnect", socket.id, socket.roomId, sockets);
 
-//         if (socket.roomId)
-//         {
-//             socket.to(socket.roomId).emit("message", { from: socket.id, type: "disconnected" });
-//         } 
-//     });
+        if (socket.roomId)
+        {
+            socket.to(socket.roomId).emit("message", { from: socket.id, type: "disconnected" });
+        } 
+    });
 
-//     socket.on("signal", (data) =>
-//     {
-//         console.log('signal!');
-//         console.log(data);
-//         console.log("signal", socket.id, data.room);
+    socket.on("signal", (data) =>
+    {
+        console.log('signal!');
+        console.log(data);
+        console.log("signal", socket.id, data.room);
 
-//         socket.to(data.room).emit("signal", data);
-//     });
+        socket.to(data.room).emit("signal", data);
+    });
 
-//     socket.on("message", (message) =>
-//     {
-//         console.log('message!');
-//         console.log(data);
-//         console.log("message", message.data.type);
+    socket.on("message", (message) =>
+    {
+        console.log('message!');
+        console.log(data);
+        console.log("message", message.data.type);
 
-//         socket.to(message.room).emit("message", message.data);
-//     });
-// });
+        socket.to(message.room).emit("message", message.data);
+    });
+});
