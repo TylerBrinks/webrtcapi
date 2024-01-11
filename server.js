442a6afd2aa7365778ee5b94f31de4e40d06a34a
Tyler Brinks <tylerbrinks@hotmail.com>
2024-01-10 20:37:34 -0700

    io req

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //
 const app = require("express")();
 const server = require("http").Server(app);
-// const io = require("socket.io")(server);
+const io = require("socket.io")(server);
 
 //
 const peerConfig = require("./peer-config");
